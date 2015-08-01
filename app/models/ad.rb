abedc3c73a4cceeff4fa36d0b13b526dd3172153
Jordan Farnworth <jfarnworth@instructure.com>
2015-08-01 16:50:12 -0600

    fixed my crazy quotes

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,5 +3,5 @@ class Ad < ActiveRecord::Base
 
   belongs_to :entity
   has_many :ad_applications
-  has_many :applicants, through: :ad_applications, foreign_key: :band_id, class_name: ‘Band’
+  has_many :applicants, through: :ad_applications, foreign_key: :band_id, class_name: 'Band'
 end
